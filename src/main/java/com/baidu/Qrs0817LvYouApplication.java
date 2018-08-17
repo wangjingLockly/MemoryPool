5d66b64728d22a8a4991ec8bf8e64a95e6903b19
ASUS <ASUS@DESKTOP-NEJC23T>
2018-08-17 21:21:06 +0800

    1

---
+++ src/main/java/com/baidu/Qrs0817LvYouApplication.java
@@ -0,0 +1,12 @@
+package com.baidu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qrs0817LvYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qrs0817LvYouApplication.class, args);
+	}
+}
